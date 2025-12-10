f28f233b5ad346676d6b4db4c52de1e64d6d91c4
bondrewd0 <agustinchan@hotmail.com.ar>
2025-12-10 20:59:21 -0300

    arreglo btn exit

--- Scenes/LevelElements/dialogue_box.gd
+++ Scenes/LevelElements/dialogue_box.gd
@@ -4,7 +4,6 @@ var text_tween:Tween
 @onready var dialogue: RichTextLabel = $Dialogue
 @onready var prev_scene: TextureButton = $PrevScene
 @onready var next_scene: TextureButton = $NextScene
-@onready var exit: Button = $Exit
 
 signal next_pressed
 signal prev_pressed
@@ -15,7 +14,6 @@ func reset_text():
 	dialogue.visible_ratio=0.0
 
 func play_text():
-	print(dialogue.text.length())
 	dialogue.text=Text2Display
 	text_tween=create_tween()
 	text_tween.tween_property(dialogue,"visible_ratio",1.0,2)
@@ -25,7 +23,3 @@ func _on_prev_scene_pressed() -> void:
 
 func _on_next_scene_pressed() -> void:
 	next_pressed.emit()
-
-
-func _on_exit_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Menus/MainMenuUI.tscn")
